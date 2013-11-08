cb89a0241fd13e8017d59e0af4d1589539c43c87
My Tec Bits <info@mytecbits.com>
2013-11-08 13:13:06 -0600

    Initial commit

---
+++ MyTecBits-MVC5-Foundation4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTecBits_MVC5_Foundation4.Startup))]
+namespace MyTecBits_MVC5_Foundation4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
